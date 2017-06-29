c3d56fc3160eb070b251506cae5ecec3d9c08047
Shubham Aneja <shubham.aneja@3pillar.corp>
2017-06-29 17:05:18 +0530

    jenkins testing commit message

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
 class App extends Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div className="App" style={{}}>
-          <div className={"div_row div_align_center"}><span>Name</span><TextField /></div>
-          <div className={"div_row div_align_center"}><span>Email</span><TextField type={"email"}/></div>
-          <RaisedButton label={"Submit"}/>
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App" style={{}}>
+                    <div className={"div_row div_align_center"}>
+                        <TextField floatingLabelText="Name"/>
+                    </div>
+                    <div className={"div_row div_align_center"}>
+                        <TextField type={"email"}  floatingLabelText="Email"/>
+                    </div>
+                    <RaisedButton label={"Submit"}/>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
